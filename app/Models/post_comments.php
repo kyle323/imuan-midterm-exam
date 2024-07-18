3564fbec95c7a419b241e2137041b75bb835fa7d
kyle323 <imuankg.925.stud@cdd.edu.ph>
2024-07-18 12:28:49 +0800

    initial commit kyle

---
+++ app/Models/post_comments.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+use app\models\user;
+use app\models\post_comment;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post_comments extends Model
+{
+    use HasFactory;
+}
